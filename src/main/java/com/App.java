f1d2a68a0d596ba9e5d3af4233e9a78910a685ac
Akash <akash300383@gmail.com>
2021-01-06 20:50:48 +0530

    Done once again change

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-System.out.println("Akash Kale Here done some work");
+System.out.println("Akash Kale Here done some work - One again done some changes");
         System.out.println( "Hello .......World!.....Hello.Hello...." );
     System.out.println( "Maven with Jenkin with Event concept" );
     System.out.println( "Maven with Jenkin with Event concept I Done some changes.....Done once again" );	
