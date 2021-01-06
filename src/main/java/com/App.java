46ba1e8cb5f7e77ec3ba016867360f32107c23de
Akash <akash300383@gmail.com>
2021-01-06 19:16:50 +0530

    Some changes done

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-    System.out.println( "Maven with Jenkin with Event concept" );	
+    System.out.println( "Maven with Jenkin with Event concept" );
+    System.out.println( "Maven with Jenkin with Event concept I Done some changes" );		
     }
 }
