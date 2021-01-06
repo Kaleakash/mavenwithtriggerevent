285435e5e7668ef3033819b8a61962ca652c71c2
Akash <akash300383@gmail.com>
2021-01-06 21:00:39 +0530

    Added New contents

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -8,11 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-System.out.println("Akash Kale Here d...one. some work - One again done some changes");
-        System.out.println( "Hello .......World!.....Hello.Hello...." );
-    System.out.println( "Maven with Jenkin with Event concept" );
-    System.out.println( "Maven with Jenkin with Event concept I Done some changes.....Done once again" );	
-        System.out.println("Small changes done in Git hub projects");
-        System.out.println("I Done Anot....her Change.....");
+	System.out.println("Welcome to Maven with Jenkin with Event Triggered");
     }
 }
