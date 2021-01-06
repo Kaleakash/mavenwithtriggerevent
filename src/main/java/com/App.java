1d4b1a1fd885127a9496d17580370b56061ec3c5
Akash <akash300383@gmail.com>
2021-01-06 20:45:00 +0530

    Done some changes

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello .......World!.....Hello....." );
+        System.out.println( "Hello .......World!.....Hello.Hello...." );
     System.out.println( "Maven with Jenkin with Event concept" );
     System.out.println( "Maven with Jenkin with Event concept I Done some changes.....Done once again" );	
         System.out.println("Small changes done in Git hub projects");
