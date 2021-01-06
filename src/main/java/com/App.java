8b68505ad6f4d042d96b47d041cb27bc02e8d568
Kaleakash <34272796+Kaleakash@users.noreply.github.com>
2021-01-06 20:18:10 +0530

    Update App.java

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -10,7 +10,7 @@ public class App
     {
         System.out.println( "Hello World!.....Hello" );
     System.out.println( "Maven with Jenkin with Event concept" );
-    System.out.println( "Maven with Jenkin with Event concept I Done some changes" );	
+    System.out.println( "Maven with Jenkin with Event concept I Done some changes.....Done once again" );	
         System.out.println("Small changes done in Git hub projects");
         System.out.println("I Done Another Change.....");
     }
